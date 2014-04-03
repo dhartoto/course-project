527b7ab4c77071da3ca1cfd1df90ce2c98253185
Dito <dhartoto@yahoo.com>
2014-04-03 14:26:10 +0700

    Pre-course exercises

---
+++ Ex/Ex6.rb
@@ -0,0 +1,12 @@
+# Angry boss.
+puts "What do you want!!"
+me = gets.chomp
+boss = 'WHADDAYA MEAN "' + me.upcase + '"!?! YOU\'RE FIRED!!!'
+puts boss
+ 
+ # Table of content.
+ puts "Table of Contents".center(50)
+line_width = 20
+puts ("Chapter 1:".ljust(line_width) + "Getting started".center(line_width) + "page 1".rjust(line_width))
+puts ("Chapter 2:".ljust(line_width) + "Numbers".center(line_width) + "page 9".rjust(line_width))
+puts ("Chapter 3:".ljust(line_width) + "Letters".center(line_width) + "page 13".rjust(line_width))
